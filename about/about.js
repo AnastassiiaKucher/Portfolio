c0733fb02f56f7cf27e314437392ba46232160b3
AnastassiiaKucher <anastasielisson@gmail.com>
2025-10-22 17:04:37 +0200

    first commit

---
+++ about/about.js
@@ -0,0 +1,3 @@
+
+gsap.from('.black_menu', { duration: 2, delay: 2, y: '10vw', ease: 'Bounce.easeOut',opacity: 0  })
+
---
+++ projects/script_p2.js
@@ -0,0 +1,3 @@
+
+gsap.from('.black_menu', { duration: 2, delay: .3, y: '10vw', ease: 'Bounce.easeOut',opacity: 0  })
+
